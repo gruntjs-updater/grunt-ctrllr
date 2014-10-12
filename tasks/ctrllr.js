b2a6651441886b37803c55615723f7816444528e
ishmaelthedestroyer <ishmaelthedestroyer@gmail.com>
2014-10-12 16:15:39 -0700

    load server from path

--- tasks/ctrllr.js
+++ tasks/ctrllr.js
@@ -8,43 +8,49 @@
 
 'use strict';
 
+var CTRLLR = require('ctrllr');
+var path = require('path');
+
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('ctrllr', 'Grunt task for running ctrllr API tests.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    grunt.registerMultiTask('ctrllr', 'Grunt task for running ctrllr API tests.', function() {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var done = this.async();
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+        var options = this.options({
+
+        });
+
+        if (typeof options.server === 'string') {
+            options.server = require(path.resolve(__dirname + '/../' + options.server));
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
+        var c = new CTRLLR(options);
 
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+        // Iterate over all specified file groups.
+        this.files.forEach(function(f) {
+            f.src.filter(function(filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function(filepath) {
+                // Read file source, add to tests
+                // var test = grunt.file.read(filepath);
+                console.log('Adding test.', filepath);
 
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+                var test = require(path.resolve(__dirname + '/../' + filepath));
+                c.add(test);
+                return test;
+            });
+        });
+
+        c.start();
     });
-  });
-
 };
