1d5bcd9a80289f98c46ee742dd10e16d5d3fcae4
ishmaelthedestroyer <ishmaelthedestroyer@gmail.com>
2015-02-13 13:18:44 -0800

    updated ctrllr; version bump -> 0.1.9

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,78 +10,78 @@
 
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-            jshint: {
-                all: [
-                    'Gruntfile.js',
-                    'tasks/*.js',
-                    '<%= nodeunit.tests %>'
-                ],
-                options: {
-                    jshintrc: '.jshintrc'
-                }
-            },
-
-            // Before generating any new files, remove any previously-created files.
-            clean: {
-                tests: ['tmp']
-            },
-
-            ctrllr: {
-                all: {
-                    options: {
-                        server: 'test/fixtures/server'
-                    },
-                    src: [
-                        'test/fixtures/spec-*.js'
-                    ]
-                },
-                spec2: {
-                    options: {
-                        server: 'test/fixtures/server'
-                    },
-                    src: [
-                        'test/fixtures/spec-2.js'
-                    ]
-                },
-                custom: {
-                    options: require('./test/fixtures/config'),
-                    files: {
-                        src: [
-                            'test/fixtures/spec-*.js'
-                        ]
-                    }
-                }
-            },
-
-            // Unit tests.
-            nodeunit: {
-                tests: ['test/*_test.js']
-            }
-
+  // Project configuration.
+  grunt.initConfig({
+      jshint: {
+        all: [
+          'Gruntfile.js',
+          'tasks/*.js',
+          '<%= nodeunit.tests %>'
+        ],
+        options: {
+          jshintrc: '.jshintrc'
         }
-    );
+      },
 
-    // Actually load this plugin's task(s).
-    grunt.loadTasks('tasks');
+      // Before generating any new files, remove any previously-created files.
+      clean: {
+        tests: ['tmp']
+      },
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+      ctrllr: {
+        all: {
+          options: {
+            server: 'test/fixtures/server'
+          },
+          src: [
+            'test/fixtures/spec-*.js'
+          ]
+        },
+        spec2: {
+          options: {
+            server: 'test/fixtures/server'
+          },
+          src: [
+            'test/fixtures/spec-2.js'
+          ]
+        },
+        custom: {
+          options: require('./test/fixtures/config'),
+          files: {
+            src: [
+              'test/fixtures/spec-*.js'
+            ]
+          }
+        }
+      },
 
-    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-    // plugin's task(s), then test the result.
-    grunt.registerTask('test', [
-        'clean',
-        'ctrllr',
-        'nodeunit'
-    ]);
+      // Unit tests.
+      nodeunit: {
+        tests: ['test/*_test.js']
+      }
 
-    // By default, lint and run all tests.
-    grunt.registerTask('default', [
-        'jshint',
-        'test'
-    ]);
+    }
+  );
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', [
+    'clean',
+    'ctrllr',
+    'nodeunit'
+  ]);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', [
+    'jshint',
+    'test'
+  ]);
 };
